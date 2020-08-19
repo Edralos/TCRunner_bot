e5e060434ecb7bc1a2cb49cde37613bf9f74546e
Edralos <titimornet@gmail.com>
2020-08-19 17:50:37 +0200

    Update log.py

--- log.py
+++ log.py
@@ -1,73 +1,119 @@
-import requests
-
+from discord_webhook import DiscordWebhook
 
 logFileName = None
 logFile = None
-discordWebHookUrl = None
+discordWebhookUrl = None
 
 ##File
 
 
 
-def setlogFile(logFileName):
+def setlogFile(logfileName):
+    global logFile
     if logFile is not None:
         logFile.close()
-    logFile = open(logFileName, "a")
-    logFileName = logFileName
+    global logFileName
+    logFile = open(logfileName, "a")
+    logFileName = logfileName
     return
-    
+
 def closelogFile():
+    global logFile
     if logFile is not None:
         logFile.close()
+    global logFileName
     logFileName = None
     return
 
-def writeInitLogFile(config):
+def _writeInitLogFile(config):
     try:
         #list of strategies
         strats = config["strategies"]
         config.pop["strategies"]
-        
-        file.write("Config:\n")
+
+        logFile.write("Config:\n")
         #global config
         for k,v in config.items():
-            file.write(k + " : " + str(v) + "\n")
-        file.write("\nCurrency config:\n")
+            logFile.write(k + " : " + str(v) + "\n")
+        logFile.write("\nCurrency config:\n")
         for i in strats:
             for k,v in strats[i]:
-                file.write("\t" + k + " : " + str(v) + "\n")
-            file.write("\n")
-        file.write("\n\n")
-        
+                logFile.write("\t" + k + " : " + str(v) + "\n")
+            logFile.write("\n")
+        logFile.write("\n\n")
+
     except:
         print("error logfile")
     finally:
         return
 
-def writeFileNecklineReached(neckType, currency, val):
+def _writeFileNecklineReached(neckType, currency, val):
     try:
-        file.write( "Neckline " + neckType + " ("+ str(val) + ") reached for :" + currency + "\nNext neckline breach won't be considered until units of this currency are sold\n")
+        logFile.write( "Neckline " + neckType + " ("+ str(val) + ") reached for :" + currency + "\nNext neckline breach won't be considered until units of this currency are sold\n")
     except:
         print("error logfile")
     finally:
         return
 
-def writeFileLoan(currency, amount):
+def _writeFileLoan(currency, amount):
     try:
-        file.write("loan : " + str(amount) + " " + currency)
+        logFile.write("loan : " + str(amount) + " " + currency)
     except:
         print("error logfile")
     finally:
         return
-        
-    
 
-def writeFileText(txt):
+
+
+def _writeFileText(txt):
     try:
-        file.write(txt + "\n")
+        logFile.write(txt + "\n")
     except:
         print("error logfile")
     finally:
         return
-    
 
+def _writeFilePurchase(amount, cost, cryptoCurrency, exchangeCurrency):
+    try:
+        logFile.write("Purchase : " + str(amount) + " " +cryptoCurrency + " for " + str(cost) + " " + exchangeCurrency+ "\n")
+    except:
+        print("error logfile")
+    finally:
+        return
+
+
+def _writeFileSale(amount, cryptoCurrency, benefits, exchangeCurrency, percentageDifference):
+    try:
+        if percentageDifference <0:
+            sign = "-"
+        else:
+            sign = "+"
+        logFile.write("Sold : " + str(amount) + " " + cryptoCurrency + " for " + str(benefits) +" " + exchangeCurrency + " " + sign + str(percentageDifference) + "%")
+    except:
+        print("error logfile")
+    finally:
+        return
+
+
+def _writeFileResume(ignoredCurrencies):
+    try:
+        st = ""
+        for i in ignoredCurrencies:
+            st += ignoredCurrencies[i] + " "
+        logFile.write("Resuming. Ignored currencies are : " + st)
+    except:
+        print("error logfile")
+    finally:
+        return
+
+##Discord Webhook
+
+def setDiscordWebhookUrl(url):
+    global discordWebhookUrl
+    discordWebhookUrl = url;
+    return
+
+def writeDiscordText(text):
+    hook = DiscordWebhook(url=discordWebhookUrl, content=text)
+    response = hook.execute()
+    return
