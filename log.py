8b59cfb7fa8e99536d3b5ab7121a6e489e32e2a9
Thibaud Mornet Blanchet <thibaud.mornet-blanchet@alumni.univ-avignon.fr>
2020-09-03 22:38:09 +0200

    log terminé

--- log.py
+++ log.py
@@ -1,12 +1,13 @@
 import discord
 from DiscordBot import DBot
+import copy
+import datetime
 
 logFileName = None
 logFile = None
 bot = None
 
 ##File
-
 
 
 def setlogFile(logfileName):
@@ -30,18 +31,19 @@ def _writeInitLogFile(config):
     try:
         #list of strategies
         strats = config["strategies"]
-        config.pop["strategies"]
+        config.pop("strategies")
 
-        logFile.write("Config:\n")
+        logFile.write(str(datetime.datetime.now()) + " : Config:\n")
         #global config
         for k,v in config.items():
             logFile.write(k + " : " + str(v) + "\n")
         logFile.write("\nCurrency config:\n")
         for i in strats:
-            for k,v in strats[i]:
+            for k,v in i.items():
                 logFile.write("\t" + k + " : " + str(v) + "\n")
             logFile.write("\n")
         logFile.write("\n\n")
+        logFile.flush()
 
     except:
         print("error logfile")
@@ -50,7 +52,8 @@ def _writeInitLogFile(config):
 
 def _writeFileNecklineReached(neckType, currency, val):
     try:
-        logFile.write( "Neckline " + neckType + " ("+ str(val) + ") reached for :" + currency + "\nNext neckline breach won't be considered until units of this currency are sold\n")
+        logFile.write(str(datetime.datetime.now()) +  " : Neckline " + neckType + " ("+ str(val) + ") reached for :" + currency + "\nNext neckline breach won't be considered until units of this currency are sold\n")
+        logFile.flush()
     except:
         print("error logfile")
     finally:
@@ -58,7 +61,8 @@ def _writeFileNecklineReached(neckType, currency, val):
 
 def _writeFileLoan(currency, amount):
     try:
-        logFile.write("loan : " + str(amount) + " " + currency)
+        logFile.write(str(datetime.datetime.now()) + " : Loan : " + str(amount) + " " + currency)
+        logFile.flush()
     except:
         print("error logfile")
     finally:
@@ -68,7 +72,8 @@ def _writeFileLoan(currency, amount):
 
 def _writeFileText(txt):
     try:
-        logFile.write(txt + "\n")
+        logFile.write(str(datetime.datetime.now()) + " : " + txt + "\n")
+        logFile.flush()
     except:
         print("error logfile")
     finally:
@@ -76,20 +81,22 @@ def _writeFileText(txt):
 
 def _writeFilePurchase(amount, cost, cryptoCurrency, exchangeCurrency):
     try:
-        logFile.write("Purchase : " + str(amount) + " " +cryptoCurrency + " for " + str(cost) + " " + exchangeCurrency+ "\n")
+        logFile.write(str(datetime.datetime.now()) + " : Purchase : " + str(amount) + " " +cryptoCurrency + " for " + str(cost) + " " + exchangeCurrency+ "\n")
+        logFile.flush()
     except:
         print("error logfile")
     finally:
         return
 
 
-def _writeFileSale(amount, cryptoCurrency, benefits, exchangeCurrency, percentageDifference):
+def _writeFileSale(amount, cryptoCurrency, benefits, exchangeCurrency, percentageDifference, difference):
     try:
         if percentageDifference <0:
             sign = "-"
         else:
             sign = "+"
-        logFile.write("Sold : " + str(amount) + " " + cryptoCurrency + " for " + str(benefits) +" " + exchangeCurrency + " " + sign + str(percentageDifference) + "%")
+        logFile.write(str(datetime.datetime.now()) + " : Sold : " + str(amount) + " " + cryptoCurrency + " for " + str(benefits) +" " + exchangeCurrency + " " + sign + str(percentageDifference) + "%. Gain: " + str(difference) +" "+ exchangeCurrency)
+        logFile.flush()
     except:
         print("error logfile")
     finally:
@@ -100,8 +107,9 @@ def _writeFileResume(ignoredCurrencies):
     try:
         st = ""
         for i in ignoredCurrencies:
-            st += ignoredCurrencies[i] + " "
-        logFile.write("Resuming. Ignored currencies are : " + st)
+            st += i + " "
+        logFile.write(str(datetime.datetime.now()) + " : Resuming. Ignored currencies are : " + st)
+        logFile.flush()
     except:
         print("error logfile")
     finally:
@@ -123,14 +131,15 @@ def _writeInitLogDiscord(config):
     res = "Config :\n"
     #list of strategies
     strats = config["strategies"]
-    config.pop["strategies"]
+    config.pop("strategies")
 
     #global config
     for k,v in config.items():
         res += k + " : " + str(v) + "\n"
     logFile.write("\nCurrency config:\n")
+
     for i in strats:
-        for k,v in strats[i]:
+        for k,v in i.items():
             res += "\t" + k + " : " + str(v) + "\n"
         res += "\n"
     _writeDiscordText(res)
@@ -149,20 +158,20 @@ def _writeDiscordPurchase(amount, cost, cryptoCurrency, exchangeCurrency):
     fields = {"Purchased": str(amount)+" " + cryptoCurrency, "Cost": str(cost)+" "+exchangeCurrency}
     bot.sendEmbed(title="Purchase", description=bot.MENTION, fields=fields, color=0xd6ff42)
 
-def _writeDiscordSale(amount, cryptoCurrency, benefits, exchangeCurrency, percentageDifference):
+def _writeDiscordSale(amount, cryptoCurrency, benefits, exchangeCurrency, percentageDifference, difference):
     if percentageDifference < 0:
         color = 0xff1414
     else:
         color = 0x19f032
-    fields = {"Sold": str(amount)+" "+cryptoCurrency, "Sold for": str(benefits)+" "+exchangeCurrency, "Percentage": "```diff\n"+str(percentageDifference) + "%\n```"}
+    fields = {"Sold": str(amount)+" "+cryptoCurrency, "Sold for": str(benefits)+" "+exchangeCurrency, "Percentage": "```diff\n"+str(percentageDifference) + "%\n```", "Gain": str(difference) +" "+exchangeCurrency}
     bot.sendEmbed(title="Sale", description=bot.MENTION, fields=fields, color=color)
 
 
 def _writeDiscordResume(ignoredCurrencies):
     st = ""
     for i in ignoredCurrencies:
-        st += ignoredCurrencies[i] + " "
-    bot.sendText(bot.MENTION+" Resuming. Ignored currencies are: "+ st)
+        st += i + " "
+    bot.sendText(" Resuming. Ignored currencies are: "+ st)
 
 
 ## general log
@@ -186,9 +195,9 @@ def writeText(text):
 
 
 def writeInitLog(config):
-    _writeInitLogFile(config=config)
+    _writeInitLogFile(config=copy.deepcopy(config))
     if bot is not None:
-        _writeInitLogDiscord(config=config)
+        _writeInitLogDiscord(config=copy.deepcopy(config))
 
 
 def writeNecklineReached(neckType, currency, val):
@@ -201,10 +210,10 @@ def writePurchase(amount, cost, cryptoCurrency, exchangeCurrency):
     if bot is not None:
         _writeDiscordPurchase(amount=amount, cost=cost, cryptoCurrency=cryptoCurrency, exchangeCurrency=exchangeCurrency)
 
-def writeSale(amount, cryptoCurrency, benefits, exchangeCurrency, percentageDifference):
-    _writeFileSale(amount=amount,cryptoCurrency=cryptoCurrency,benefits=benefits,exchangeCurrency=exchangeCurrency, percentageDifference=percentageDifference)
+def writeSale(amount, cryptoCurrency, benefits, exchangeCurrency, percentageDifference, difference):
+    _writeFileSale(amount=amount,cryptoCurrency=cryptoCurrency,benefits=benefits,exchangeCurrency=exchangeCurrency, percentageDifference=percentageDifference, difference=difference)
     if bot is not None:
-        _writeDiscordSale(amount=amount,cryptoCurrency=cryptoCurrency,benefits=benefits,exchangeCurrency=exchangeCurrency, percentageDifference=percentageDifference)
+        _writeDiscordSale(amount=amount,cryptoCurrency=cryptoCurrency,benefits=benefits,exchangeCurrency=exchangeCurrency, percentageDifference=percentageDifference, difference=difference)
 
 
 def writeResume(ignoredCurrencies):
